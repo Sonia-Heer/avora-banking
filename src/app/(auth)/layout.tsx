b4bfe37e893e3fa7a806a64abd4b3f258f479bd5
Sonia Heer <soniaheer25@yahoo.com>
2025-10-30 17:47:43 +0400

    Finalise project

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function RootLayout({
@@ -11,8 +12,17 @@ export default async function RootLayout({
   if (loggedIn) redirect("/");
 
   return (
-    <main className="grid md:grid-cols-2 h-screen ">
-      <div className="auth-asset" />
+    <main className="grid md:grid-cols-2 h-screen">
+      <div className="relative hidden md:block bg-brand-secondary">
+        <Image
+          src="/placeholder.png"
+          alt="Banking illustration"
+          fill
+          className="object-bottom"
+          priority
+        />
+      </div>
+
       {children}
     </main>
   );
