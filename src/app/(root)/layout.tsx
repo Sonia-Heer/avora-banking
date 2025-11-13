f885ade72dd73d772fc59e473d6638feccc8cda9
Sonia Heer <soniaheer25@yahoo.com>
2025-11-13 15:11:16 +0400

    Extend background color on my banks page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -13,7 +13,7 @@ export default async function RootLayout({
   if (!loggedIn) redirect("/sign-in");
 
   return (
-    <main className="flex w-full bg-background">
+    <main className="flex w-full min-h-screen bg-background">
       <div className="flex size-full flex-col">
         <TopMenu user={loggedIn} />
         <div className="root-layout bg-foreground w-full fixed z-100">
